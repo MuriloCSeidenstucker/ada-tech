cab5957afac7b1945603c92b78ebfb8ce78c9bcd
Murilo Seidenstucker <murilocampos.004@gmail.com>
2024-12-17 10:16:17 -0300

    add Node.js environment support to ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,16 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    env: {
+      browser: true,  // Reconhece variáveis globais do navegador
+      node: true,     // Reconhece variáveis globais do Node.js
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
